de0de9871b5a2facdefbcb3f2bff68f39a451211
Sourav <dsourav@Souravs-MacBook-Air.local>
2024-03-31 13:20:07 +0530

    bdd testing

---
+++ src/test/java/com/sourav/tddbdddemo/bdd/StepDefinitions.java
@@ -0,0 +1,25 @@
+package com.sourav.tddbdddemo.bdd;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.cucumber.java.en.Given;
+
+public class StepDefinitions {
+
+    @Given("I want to test using cucumber {int} times")
+    public void I_want_to_test_using_cucumber_times(int times) {
+        assertThat(times).isGreaterThanOrEqualTo(times);
+        for (int i = 0; i < times; i++) {
+        	 System.out.println("I'm testing");
+		}
+    }
+    @Given("I want to say Hello {int} times")
+    public void sayHello(int times) {
+        for (int i = 0; i < times; i++) {
+        	 System.out.println(i + " Hello");
+		}
+    }
+    
+    
+
+}
---
+++ src/test/resources/com/sourav/tddbdddemo/bdd/sourav.feature
@@ -0,0 +1,8 @@
+Feature: Cucumber Test
+
+  Scenario: I like Cucumber And Want To Test It
+    Given I want to test using cucumber 10 times
+    
+    And I want to say Hello 5 times
+    
+    
